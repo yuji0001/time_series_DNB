eacc4028e9bf39fcff12033621cccebd59d19200
Yuji Okamoto <yuji.0001@gmail.com>
2022-08-17 05:47:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setuptools.setup(
     name="time_series_DNB",
-    version="0.1",
+    version="0.2",
     author="Yuji Okamoto",
     author_email="yuji.0001@gmail.com",
     description="DNB library for long time series data",
