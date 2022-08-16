3ec28a2cd1df9835056e202e7635be8901cdbbe2
Yuji Okamoto <yuji.0001@gmail.com>
2022-08-17 05:46:47 +0900

    change directory

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import setuptools
 import os
 
-# path = os.path.dirname(os.path.abspath(__file__))
-# shutil.copyfile(f"{path}/dmm.py", f"{path}/dmm/dmm.py")
-
 setuptools.setup(
     name="time_series_DNB",
     version="0.1",
@@ -12,14 +9,13 @@ setuptools.setup(
     description="DNB library for long time series data",
     long_description="DNB library for long time series data",
     long_description_content_type="text/markdown",
-    # package_dir={"": "src"},
     url="https://github.com/yuji0001/time_series_DNB",
     packages=setuptools.find_packages(),
-    # entry_points={
-    #     "console_scripts": [
-    #         "TS_DNB= dnb_ts.dnb_ts:main",
-    #     ],
-    # },
+    entry_points={
+        "console_scripts": [
+            "dnb_ts = dnb_tools.dnb_ts:main",
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: Apache License 2.0",
