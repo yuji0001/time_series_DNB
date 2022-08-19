ce85d484be7f1e5004690b78ab17e92b6c674310
Yuji Okamoto <yuji.0001@gmail.com>
2022-08-20 03:32:45 +0900

    update CPD

--- dnb_tools/dnb_ts.py
+++ dnb_tools/dnb_ts.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import tqdm
 from sklearn.decomposition import PCA
+import ruptures as rpt
 
 import dnb_tools
 from  numpy.lib.stride_tricks import sliding_window_view as sliding_window
@@ -62,6 +63,53 @@ def EWS_DNB(x,window_size,padding = 'online',normalization = 'straight'):
     else:
         raise NameError('select \'same\', \'online\', or \'valid\' ')
         return -1
+
+
+def CPDotsu(ews):
+    def OtsuScore(data,thresh):
+        w_0 = np.sum(data<=thresh)/data.shape[0]
+        w_1 = np.sum(data>thresh)/data.shape[0]
+        # check ideal case    
+        if (w_0 ==0) | (w_1 == 0):
+            return 0
+        mean_all = data.mean()
+        mean_0 = data[data<=thresh].mean()
+        mean_1 = data[data>thresh].mean()
+        sigma2_b =  w_0 *((mean_0 - mean_all)**2) + w_1 *((mean_1 - mean_all)**2)
+
+        return sigma2_b
+    ths = (ews.max() - ews.min()) * np.arange(0,1,1e-4) + ews.min()
+    scores = np.zeros(ths.shape[0])
+    for i in range(ths.shape[0]):
+        scores[i] = OtsuScore(ews,ths[i])
+    y_ews = np.zeros(ews.shape[0]) 
+    y_ews[ews > ths[scores.argmax()]] = 1
+
+    max_time = ews.argmax()
+    cp = max_time - y_ews[:max_time][::-1].argmin()
+    return cp
+
+@timer
+def CPD_EWS(ews,cfg = {'type':'ar','dim':2},scope_range = np.inf ):
+    max_time = ews.argmax()
+    window_size = min(scope_range,max_time)
+    ews_calc = ews[max_time - window_size:max_time]/ews[max_time - window_size:max_time].std()
+    if cfg['type'] == 'peak':
+        cp = max_time
+    elif cfg['type'] == 'ohtsu':
+        cp = max_time - window_size+ CPDotsu(ews_calc)
+    elif cfg['type'] == 'linear':
+        algo = rpt.Dynp(model = 'linear',min_size=1, jump=1).fit(ews_calc.reshape(-1,1))
+        cp = max_time - window_size + algo.predict(1)[0]
+    elif cfg['type'] == 'ar':
+        algo = rpt.Dynp(model = 'ar',params={"order": cfg['dim']}).fit(ews_calc)
+        cp = max_time - window_size+ algo.predict(1)[0]    
+    else:
+        raise NameError('select \'peak\',\'linear\',\'ar\', \'ohtsu\',')
+        return -1
+    return cp
+
+
 
 
 def main():
